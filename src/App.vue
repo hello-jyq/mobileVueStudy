baa9006dfa8c9e8a1ab51332fe0c698599314122
jyq <jinyuqi@basechina.net>
2019-04-11 15:08:51 +0800

    init my project

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="app-container">
+    <!-- header部分 -->
+    <mt-header fixed title="测试Vue小项目"></mt-header>
+
+     <!-- footer部分 -->
+     <nav class="mui-bar mui-bar-tab">
+			<a class="mui-tab-item mui-active" href="#tabbar">
+				<span class="mui-icon mui-icon-home"></span>
+				<span class="mui-tab-label">首页</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-chat">
+				<span class="mui-icon mui-icon-email"><span class="mui-badge">9</span></span>
+				<span class="mui-tab-label">消息</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-contact">
+				<span class="mui-icon mui-icon-contact"></span>
+				<span class="mui-tab-label">通讯录</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-map">
+				<span class="mui-icon mui-icon-gear"></span>
+				<span class="mui-tab-label">设置</span>
+			</a>
+		</nav>
+  </div>
+</template>
+
+<script>
+</script>
+
+
+<style  scoped>
+.container{
+  padding-top: 40px;
+}
+</style>
